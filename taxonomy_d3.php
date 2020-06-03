136938d8a8cdde57920816211acce5291b948044
Gerald Wagner <mail@gerald-wagner.com>
2020-06-03 17:41:39 +0200

    feat: Display taxonomy name in tree

--- taxonomy_d3.php
+++ taxonomy_d3.php
@@ -8,24 +8,23 @@ Author: Gerald Wagner
 */
 
 
-function taxonomytree_shortcode($atts) {
+function taxonomytree_shortcode( $atts ) {
 
 	$atts = shortcode_atts( array(
 		'post_type'  => 'posts',
 		'taxonomy'   => 'category'
-	), $atts);
+	), $atts );
 
  	// save 'post_type' and 'taxonomy' in wp_options to access later
-	update_option("tree_post_type", $atts['post_type']);
-	update_option("tree_taxonomy", $atts['taxonomy']);
+	update_option( 'tree_post_type', $atts['post_type'] );
+	update_option( 'tree_taxonomy',  $atts['taxonomy'] );
 
-	return '<div id ="categorytree"></div>' ;
-
+	return "<div id ='categorytree'></div>";
 }
 
 add_shortcode( 'taxonomy_d3', 'taxonomytree_shortcode' );
 
-function categoryd3tree_scripts() {
+function taxonomytree_scripts( ) {
 
 	wp_register_script( 'categoryd3tree_js', plugins_url( 'tree.js', __FILE__ ), array( 'd3_js' ) );
 	wp_enqueue_script(  'categoryd3tree_js' );
@@ -36,38 +35,41 @@ function categoryd3tree_scripts() {
 	wp_register_style( 'style_css', plugins_url( 'style.css', __FILE__ ) );
     wp_enqueue_style(  'style_css' );
 
-	// declare the URL to the file that handles the AJAX request (wp-admin/admin-ajax.php)
+	// declare the URL to the file that handles the AJAX request ( wp-admin/admin-ajax.php )
 	wp_localize_script( 'categoryd3tree_js', 'MyAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
 }
 
-add_action( 'wp_footer', 'categoryd3tree_scripts' );
+add_action( 'wp_footer', 'taxonomytree_scripts' );
 
-function categorytree_callback() {
+function categorytree_callback( ) {
 
-	$tree_taxonomy = get_option('tree_taxonomy');
+	$tree_taxonomy      = get_option( 'tree_taxonomy' );
+	$tree_taxonomy_arr  = get_taxonomy( $tree_taxonomy );
+	$tree_taxonomy_name = $tree_taxonomy_arr->labels->singular_name;
+
 	//process plugin
 	$tree = array(
 		'parent'      => -1,
-		'name'        => $tree_taxonomy, // TODO: Namen andern
+		'name'        => $tree_taxonomy_name,
 	);
 
 	// generate the response
-	//$tree['children']=buildtree();
-	$tree['children'] = buildrectree(0);
-	$response = json_encode($tree);
+	//$tree['children']=buildtree( );
+	$tree['children'] = buildrectree( 0 );
+	$response = json_encode( $tree );
 
 	// response output
-	header("Content-Type: application/json");
+	header( "Content-Type: application/json" );
 	echo $response;
-	die();
+	die( );
 }
 
-function buildrectree($root) {
+function buildrectree( $root ) {
 
-	$tree_post_type = get_option("tree_post_type");
-	$tree_taxonomy = get_option("tree_taxonomy");
+	$tree_post_type = get_option( 'tree_post_type' );
+	$tree_taxonomy  = get_option( 'tree_taxonomy' );
 	/*
-	$tree_term_order = get_option("tree_category_$l1cat->term_id");
+	$tree_term_order = get_option( "tree_category_$l1cat->term_id" );
 	*/
 
 	$args = array(
@@ -79,13 +81,14 @@ function buildrectree($root) {
 		'taxonomy'    => $tree_taxonomy,
 	);
 
-	$l1cats = get_categories($args);
+	$l1cats = get_categories( $args );
 
-	foreach($l1cats as $l1cat) {
-		$l1cat->children=buildrectree($l1cat->term_id);
+	foreach ( $l1cats as $l1cat ) {
+
+		$l1cat->children = buildrectree( $l1cat->term_id );
 
 		// push blogposts into $tree in category of last level
-		if(empty ($l1cat->children)) {
+		if ( empty ( $l1cat->children ) ) {
 			$l1posts = get_posts( array(
 				'post_type'    => $tree_post_type,
 				'meta_key'     => 'tree_order',
@@ -98,34 +101,34 @@ function buildrectree($root) {
 					)
 				)
 			));
-			foreach($l1posts as $l1post) {
-				$l1post->name=$l1post->post_title;
-				$l1cat->children[]=$l1post; //TODO: Wie ist hier das spacing sinnvoll?
+
+			foreach ( $l1posts as $l1post ) {
+
+				$l1post->name      = $l1post->post_title;
+				$l1cat->children[] = $l1post;
 			}
 		}
 
 		// push $color into $l1cat of first level
-		$cat_color= tree_color_get_term_meta( $l1cat->term_id, true );
+		$default   = '#ffffff';
+		$cat_color = tree_color_get_term_meta( $l1cat->term_id, true );
 
-		if (0 == $l1cat->parent) {
-			if (isset($cat_color) && $cat_color != '') {
+		if ( 0 == $l1cat->parent ) {
+			if ( isset( $cat_color ) && $cat_color != '' ) {
 				$l1cat->taxonomy_color = $cat_color;
 			} else { //default color
-				$l1cat->taxonomy_color = '#000000';
+				$l1cat->taxonomy_color = $default;
 			}
 		}
 
 		// push $l1cat innto $tree
-		$tree[]=$l1cat;
+		$tree[] = $l1cat;
 	}
 	return $tree;
 }
 
 add_action( 'wp_ajax_categorytree', 'categorytree_callback' );
 add_action( 'wp_ajax_nopriv_categorytree', 'categorytree_callback' );
-
-// metaboxes category_order and category_color;
-// require_once 'metabox_category.php';
 
 require_once 'metabox_color.php';
 require_once 'metabox_order.php';
